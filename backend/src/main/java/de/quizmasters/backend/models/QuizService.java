c05b45f2a088e316bce948ad63e48cb7d87f52b6
saman1357 <saman1357@yahoo.com>
2023-06-28 12:23:29 +0200

    fix...

--- backend/src/main/java/de/quizmasters/backend/models/QuizService.java
+++ backend/src/main/java/de/quizmasters/backend/models/QuizService.java
@@ -1,7 +1,6 @@
 package de.quizmasters.backend.models;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
