be6323923364f12b7e6f0b103538c09a0f60a8ce
Tobias Schweizer <tobias-schweizer@gmx.de>
2023-06-30 11:55:20 +0200

    Changes by sahedwahedi

--- backend/src/test/java/de/quizmasters/backend/models/IdServiceTest.java
+++ backend/src/test/java/de/quizmasters/backend/models/IdServiceTest.java
@@ -3,7 +3,6 @@ package de.quizmasters.backend.models;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class IdServiceTest {
 
