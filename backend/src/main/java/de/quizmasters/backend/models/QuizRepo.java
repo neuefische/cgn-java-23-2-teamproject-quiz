16a6442e0b637c25529bcc76eb2cebb738dddde0
saman1357 <saman1357@yahoo.com>
2023-07-04 15:58:13 +0200

    add multiple choice answers and updated Quiz-Model and Repo

--- backend/src/main/java/de/quizmasters/backend/models/QuizRepo.java
+++ backend/src/main/java/de/quizmasters/backend/models/QuizRepo.java
@@ -9,13 +9,13 @@ import java.util.List;
 @Repository
 public class QuizRepo {
     private final List<Quiz> quizList = new ArrayList<>(List.of(
-            new Quiz( "123","Sind Giraffen größer als Hunde?", new ArrayList<Answer>( List.of(
+            new Quiz( "123","Sind Giraffen größer als Hunde?", new ArrayList<>( List.of(
                     new Answer("Ja", true),
                     new Answer("Nein", false),
                     new Answer("Vielleicht", false),
                     new Answer("Keine Ahnung", false)
                     ))),
-            new Quiz( "456","Sind Hunde schneller als Schnecken?", new ArrayList<Answer>( List.of(
+            new Quiz( "456","Sind Hunde schneller als Schnecken?", new ArrayList<>( List.of(
                     new Answer("Ja", true),
                     new Answer("Nein", false),
                     new Answer("Vielleicht", false),
