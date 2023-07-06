b921a3be315d4474e46095d1d9b6ed7340b9d753
saman1357 <saman1357@yahoo.com>
2023-07-06 17:07:36 +0200

    merged main into multiple choice

--- backend/src/main/java/de/quizmasters/backend/models/QuizRepo.java
+++ backend/src/main/java/de/quizmasters/backend/models/QuizRepo.java
@@ -3,12 +3,6 @@ package de.quizmasters.backend.models;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-
 @Repository
 public interface QuizRepo extends MongoRepository<Quiz, String> {
-}
-
 }
--- backend/src/test/java/de/quizmasters/backend/controllers/QuizControllerTest.java
+++ backend/src/test/java/de/quizmasters/backend/controllers/QuizControllerTest.java
@@ -1,5 +1,6 @@
 package de.quizmasters.backend.controllers;
 
+import de.quizmasters.backend.models.Answer;
 import de.quizmasters.backend.models.Quiz;
 import de.quizmasters.backend.models.QuizService;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,10 +29,22 @@ class QuizControllerTest {
     @Test
     void expectAllQuizzes_whenGetAllQuizzes() throws Exception {
 
-        String expectedList= """
+        Quiz quiz1=quizService.addQuiz(new Quiz("123", "Sind Giraffen größer als Hunde?", List.of(
+                new Answer("Ja", true),
+                new Answer("Nein", false),
+                new Answer("Vielleicht", false),
+                new Answer("Keine Ahnung", false)
+        )));
+        Quiz quiz2=quizService.addQuiz(new Quiz("456", "Sind Hunde schneller als Schnecken?", List.of(
+                new Answer("Ja", true),
+                new Answer("Nein", false),
+                new Answer("Vielleicht", false),
+                new Answer("Keine Ahnung", false)
+        )));
+        String expectedList= String.format("""
                 [
                     {
-                        "id": "123",
+                        "id": "%s",
                         "question": "Sind Giraffen größer als Hunde?",
                         "answers": [
                             {
@@ -51,7 +66,7 @@ class QuizControllerTest {
                         ]
                     },
                     {
-                        "id": "456",
+                        "id": "%s",
                         "question": "Sind Hunde schneller als Schnecken?",
                         "answers": [
                             {
@@ -73,10 +88,9 @@ class QuizControllerTest {
                         ]
                     }
                 ]
-                """;
+                """,quiz1.getId(), quiz2.getId());
 
-        quizService.addQuiz(new Quiz("123", "Sind Giraffen größer als Hunde?", "Ja"));
-        quizService.addQuiz(new Quiz("456", "Sind Hunde schneller als Schnecken?", "Ja"));
+
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/quiz"))
 
@@ -145,9 +159,23 @@ class QuizControllerTest {
     @DirtiesContext
     void expectUpdatedQuiz_whenUpdateQuiz() throws Exception {
 
-        String expectedQuiz= """
+        Quiz testQuiz1 = new Quiz("123", "Sind Giraffen größer als Hunde?", List.of(
+                new Answer("Ja", true),
+                new Answer("Nein", false),
+                new Answer("Vielleicht", false),
+                new Answer("Keine Ahnung", false)
+        ));
+        Quiz testQuiz2 = new Quiz("456", "Sind Hunde schneller als Schnecken?", List.of(
+                new Answer("Ja", true),
+                new Answer("Nein", false),
+                new Answer("Vielleicht", false),
+                new Answer("Keine Ahnung", false)
+        ));
+        quizService.addQuiz(testQuiz1);
+        quizService.addQuiz(testQuiz2);
+        String expectedQuiz= String.format("""
                 {
-                        "id": "123",
+                        "id": "%s",
                         "question": "Welches Tier hat Streifen?",
                         "answers": [
                             {
@@ -168,18 +196,9 @@ class QuizControllerTest {
                             }
                         ]
                     }
-                """;
-        Quiz testQuiz1 = new Quiz("123", "Sind Giraffen größer als Hunde?", "Ja");
-        Quiz testQuiz2 = new Quiz("456", "Sind Hunde schneller als Schnecken?", "Ja");
-        quizService.addQuiz(testQuiz1);
-        quizService.addQuiz(testQuiz2);
-        String expectedQuiz = String.format("""
-                                            {
-                                             "id": "%s",
-                                              "question": "Welches Tier hat Streifen?",
-                                                "answer": "Zebra"
-                                                }
                 """, testQuiz1.getId());
+
+
 
         mockMvc.perform(MockMvcRequestBuilders.put(String.format("/api/quiz/%s",testQuiz1.getId()))
                         .contentType(MediaType.APPLICATION_JSON).content(String.format("""
@@ -205,7 +224,7 @@ class QuizControllerTest {
                             }
                         ]
                     }
-                                """, testQuiz1.getId()))
+                                """, testQuiz1.getId())))
 
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(expectedQuiz));
@@ -213,8 +232,18 @@ class QuizControllerTest {
 
     @Test
     void expectListWithoutQuizToDelete_whenDeleteQuiz() throws Exception {
-        Quiz testQuiz1 = new Quiz("123", "Sind Giraffen größer als Hunde?", "Ja");
-        Quiz testQuiz2 = new Quiz("456", "Sind Hunde schneller als Schnecken?", "Ja");
+        Quiz testQuiz1 = new Quiz("123", "Sind Giraffen größer als Hunde?", List.of(
+                new Answer("Ja", true),
+                new Answer("Nein", false),
+                new Answer("Vielleicht", false),
+                new Answer("Keine Ahnung", false)
+        ));
+        Quiz testQuiz2 = new Quiz("456", "Sind Hunde schneller als Schnecken?", List.of(
+                new Answer("Ja", true),
+                new Answer("Nein", false),
+                new Answer("Vielleicht", false),
+                new Answer("Keine Ahnung", false)
+        ));
         quizService.addQuiz(testQuiz1);
         quizService.addQuiz(testQuiz2);
         String expectedList = String.format("""
@@ -243,7 +272,7 @@ class QuizControllerTest {
                     }
                                           ]
                                             
-                """, testQuiz2.getId();
+                """, testQuiz2.getId());
 
 
         mockMvc.perform(MockMvcRequestBuilders.delete(String.format("/api/quiz/%s", testQuiz1.getId())))
