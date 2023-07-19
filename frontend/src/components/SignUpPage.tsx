923e89fefb939a013c6e65ba067242d5ec5fe2e4
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-19 16:48:07 +0200

    add back button to sign-up page

--- frontend/src/components/SignUpPage.tsx
+++ frontend/src/components/SignUpPage.tsx
@@ -1,10 +1,18 @@
 import UserNamePasswordInput from "./UserNamePasswordInput.tsx";
+import {IconButton} from "@mui/material";
+import {ArrowBack} from "@mui/icons-material";
+import {Link} from "react-router-dom";
 type Props={
     onSignUp:(username: string, password: string)=>void
 }
 function SignUpPage(props:Props) {
     return (
         <>
+            <Link to={"/"}>
+                <IconButton className={"back-button-login"} color={"secondary"}>
+                    <ArrowBack/>
+                </IconButton>
+            </Link>
             <UserNamePasswordInput onLoginSubmit={props.onSignUp} loginSubmit={false}/>
         </>
     );
